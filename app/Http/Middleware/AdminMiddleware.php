1139dda027d93bd9eddd408b3afd42018e235486
wilsongachungi <gachungiwilson91@gmail.com>
2025-04-30 08:54:14 +0300

    creating a middleware for restricting access to admin

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Support\Facades\Auth;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->isAdmin === 1){
+            return $next($request);
+        }
+
+        return redirect()->route('home')->with('error','Access denied. You are not an admin');
+
+    }
+}
